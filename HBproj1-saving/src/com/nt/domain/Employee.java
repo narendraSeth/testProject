7ace41d70508ec204c6badb170ff42cd67ea626d
narendra <narendra.seth2020@gmail.com>
2019-07-28 22:24:53 +0530

    First Commit1

---
+++ HBproj1-saving/src/com/nt/domain/Employee.java
@@ -0,0 +1,36 @@
+package com.nt.domain;
+//Domain class is POJO class;
+public class Employee 
+{
+   //	properties
+	private int eid;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+}
